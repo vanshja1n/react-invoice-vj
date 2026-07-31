ebbb8fb81b432ff7c865cd57493f9980977ec1e3
Vansh Jain <95092012+vanshja1n@users.noreply.github.com>
2026-08-01 04:19:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://react-invoice-vj.vercel.app'
+  ],
   credentials: true,
 }));
 
